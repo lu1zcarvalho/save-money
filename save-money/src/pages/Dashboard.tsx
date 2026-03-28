855b36b762f103d741f0c41a3b06202bf7fc4fa0
Luiz Augusto <zcarvalho722@gmail.com>
2026-03-28 12:46:00 -0300

    refactor: reorganize Dashboard

--- save-money/src/pages/Dashboard.tsx
+++ save-money/src/pages/Dashboard.tsx
@@ -1,10 +1,21 @@
 function Dashboard() {
-  return (
-  <main>
-    <h1>Controle Financeiro</h1>
-    <p>Bem-vindo ao seu painel financeiro.</p>
-  </main>
-  );
+    return (
+        <main>
+            <h1>Controle Financeiro</h1>
+
+            <section>
+                <h2>Resumo financeiro</h2>
+                <p>Entradas: R$ 0,00</p>
+                <p>Saídas: R$ 0,00</p>
+                <p>Saldo: R$ 0,00</p>
+            </section>
+
+            <section>
+                <h2>Transações recentes</h2>
+                <p>Nenhuma transação registrada.</p>
+            </section>
+        </main>
+    );
 }
 
 export default Dashboard;
