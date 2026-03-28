20b43067918b15059637d4723cb2064504b28c7c
Luiz Augusto <zcarvalho722@gmail.com>
2026-03-28 12:38:15 -0300

    feat: add transaction

---
+++ save-money/src/types/transaction.ts
@@ -0,0 +1,9 @@
+export type TransactionType = 'income' | 'expense';
+
+export interface Transaction {
+  id: string;
+  description: string;
+  amount: number;
+  type: TransactionType;
+  date: Date;
+}
