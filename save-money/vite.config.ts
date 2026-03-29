758672190a2adeb9b44fdf40e7eeb9d92fa4248e
Luiz Augusto <zcarvalho722@gmail.com>
2026-03-28 21:38:35 -0300

    fix: fixing host

--- save-money/vite.config.ts
+++ save-money/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] })
   ],
   server: {
+    host: "127.0.0.1",
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3333',
